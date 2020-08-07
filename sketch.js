a57b2f80425bc3780c512c899474094d59f6edf2
Aadheesh Nandan <69255310+AadheeshN@users.noreply.github.com>
2020-08-06 22:06:05 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+var movingRect, fixedRect;
+
+function setup() {
+  createCanvas(800,400);
+  movingRect = createSprite(20, 20, 20, 20);
+  fixedRect = createSprite(400, 200, 70, 50);
+  movingRect.shapeColor = "blue";
+  fixedRect.shapeColor ="yellow";
+}
+
+function draw() {
+  background(255,255,255);  
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+
+  if(movingRect.x - fixedRect.x < movingRect.width/2 + fixedRect.width/2 && fixedRect.x - movingRect.x < movingRect.width/2 + fixedRect.width/2 && fixedRect.y - movingRect.y < movingRect.height/2 + fixedRect.height/2 && movingRect.y - fixedRect.y < movingRect.height/2 + fixedRect.height/2){
+    movingRect.shapeColor  = "red";
+    fixedRect.shapeColor = "green";
+  }else{
+    movingRect.shapeColor = "blue";
+  fixedRect.shapeColor ="yellow";
+  }
+
+  drawSprites();
+}
